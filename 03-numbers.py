ed5d0664e83bc857653d6f06f21592a10ce0d068
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-01-30 18:21:06 +0700

    init

---
+++ 03-numbers.py
@@ -0,0 +1,24 @@
+# Integer => whole number
+# Float => decimal number
+
+print(type(6))
+print(type(2.5))
+
+a = 5
+b = 2.4
+c = 31_000_000_000
+d = .8
+e = 128e+2
+
+print(a, b, c, d, e)
+
+# Operators
+
+print(5 + 2)
+print(5 - 2.0)  # float
+print(5 * 2)
+print(5 / 2)  # float
+print(5 // 2)  # integer
+print(5 % 2)  # remainder
+print(5 ** 2)  # exponent
+print(5 * (10 + 3))
