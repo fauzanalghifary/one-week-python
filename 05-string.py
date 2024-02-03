9dc5d5f73d0df9b9b9f24322356b2ebdda57b35f
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-03 12:31:17 +0700

    finish chapter 5

---
+++ 05-string.py
@@ -0,0 +1,27 @@
+first_name = "Jurgen"
+last_name = 'Klopp'
+
+print(first_name, last_name)
+print(type(first_name))
+
+age = 85
+print(age)
+age = "eighty five"
+print(age)
+
+print(first_name + " " + last_name)
+print(age * 2)
+
+# String Indexing
+print(first_name[0])
+print(first_name[-1])
+print(first_name[0:3])
+print(first_name[3:])
+print(first_name[:3])
+print(first_name[0:5:2])  # [start:stop:step]
+# print(first_name[100]) # IndexError: string index out of range
+
+
+# None
+user = None
+print(user)
